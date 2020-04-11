6f69fdb128faf23150a69f28a8aa2ad765741bc4
Fatma Ali <alifatma2019@gmail.com>
2020-04-11 17:10:57 +0300

    Setup TypeScript

---
+++ components/Form/FormInput.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Input, Icon } from "react-native-elements";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      paddingLeft: 8,
+      borderRadius: 40,
+      borderWidth: 1,
+      borderColor: 'rgba(110, 120, 170, 1)',
+      height: 45,
+      marginVertical: 10,
+    },
+    inputStyle: {
+      flex: 1,
+      marginLeft: 10,
+      color: 'white',
+      fontSize: 16,
+    },
+    errorInputStyle: {
+      marginTop: 0,
+      textAlign: 'center',
+      color: '#F44336',
+    }
+  });
+export const FormInput = ({ icon, refInput, ...otherProps }) => {
+    return (
+      <Input
+        {...otherProps}
+        ref={refInput}
+        inputContainerStyle={styles.inputContainer}
+        leftIcon={
+          <Icon name={icon} type={'simple-line-icon'} color="#7384B4" size={18} />
+        }
+        inputStyle={styles.inputStyle}
+        autoFocus={false}
+        autoCapitalize="none"
+        keyboardAppearance="dark"
+        errorStyle={styles.errorInputStyle}
+        autoCorrect={false}
+        blurOnSubmit={false}
+        placeholderTextColor="#7384B4"
+      />
+    );
+  };
